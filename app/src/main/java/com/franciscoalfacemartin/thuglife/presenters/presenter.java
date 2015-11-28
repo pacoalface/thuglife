930b4ee293fd83bde4e7429995f9f947fe4833c6
Francisco Alface Martin <franciscoalfacemartin@gmail.com>
2015-11-28 21:22:11 +0100

    Added base presenter

---
+++ app/src/main/java/com/franciscoalfacemartin/thuglife/presenters/presenter.java
@@ -0,0 +1,10 @@
+package com.franciscoalfacemartin.thuglife.presenters;
+
+/**
+ * Created by franciscoalfacemartin on 28/11/15.
+ */
+public interface presenter {
+    void onCreate();
+    void onResume();
+    void onPause();
+}
